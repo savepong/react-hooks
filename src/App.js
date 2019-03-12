1e9a8404dfe3d360b78ba60aec837b3014a7ee0b
Savepong <savepong.com@gmail.com>
2019-03-12 20:59:03 +0700

    Imprement useState()

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <p>
+          You click {count} times
+        </p>
+
+        <button
+          style={{
+            padding: '8px 16px',
+            borderRadius: 4,
+            fontSize: '1.25rem'
+          }}
+          onClick={() => setCount(count + 1)}
+        >
+          Click me
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
